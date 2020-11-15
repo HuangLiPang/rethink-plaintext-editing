7e45ca006f2e517fcf56afece20d85e24ab6c563
huanglipang <lh5jv@virginia.edu>
2020-11-15 14:10:48 -0500

    complete markdwon editor

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -1,17 +1,173 @@
-import React from 'react';
+import Prism from 'prismjs';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
+import { Slate, Editable, withReact } from 'slate-react';
+import { Text, createEditor } from 'slate';
+import { withHistory } from 'slate-history';
+import { css } from '@emotion/css';
 import PropTypes from 'prop-types';
+import cssEditor from './style.css';
 
-import css from './style.css';
+// eslint-disable-next-line
+;Prism.languages.markdown=Prism.languages.extend("markup",{}),Prism.languages.insertBefore("markdown","prolog",{blockquote:{pattern:/^>(?:[\t ]*>)*/m,alias:"punctuation"},code:[{pattern:/^(?: {4}|\t).+/m,alias:"keyword"},{pattern:/``.+?``|`[^`\n]+`/,alias:"keyword"}],title:[{pattern:/\w+.*(?:\r?\n|\r)(?:==+|--+)/,alias:"important",inside:{punctuation:/==+$|--+$/}},{pattern:/(^\s*)#+.+/m,lookbehind:!0,alias:"important",inside:{punctuation:/^#+|#+$/}}],hr:{pattern:/(^\s*)([*-])([\t ]*\2){2,}(?=\s*$)/m,lookbehind:!0,alias:"punctuation"},list:{pattern:/(^\s*)(?:[*+-]|\d+\.)(?=[\t ].)/m,lookbehind:!0,alias:"punctuation"},"url-reference":{pattern:/!?\[[^\]]+\]:[\t ]+(?:\S+|<(?:\\.|[^>\\])+>)(?:[\t ]+(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\)))?/,inside:{variable:{pattern:/^(!?\[)[^\]]+/,lookbehind:!0},string:/(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\))$/,punctuation:/^[\[\]!:]|[<>]/},alias:"url"},bold:{pattern:/(^|[^\\])(\*\*|__)(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^\*\*|^__|\*\*$|__$/}},italic:{pattern:/(^|[^\\])([*_])(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,lookbehind:!0,inside:{punctuation:/^[*_]|[*_]$/}},url:{pattern:/!?\[[^\]]+\](?:\([^\s)]+(?:[\t ]+"(?:\\.|[^"\\])*")?\)| ?\[[^\]\n]*\])/,inside:{variable:{pattern:/(!?\[)[^\]]+(?=\]$)/,lookbehind:!0},string:{pattern:/"(?:\\.|[^"\\])*"(?=\)$)/}}}}),Prism.languages.markdown.bold.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.italic.inside.url=Prism.util.clone(Prism.languages.markdown.url),Prism.languages.markdown.bold.inside.italic=Prism.util.clone(Prism.languages.markdown.italic),Prism.languages.markdown.italic.inside.bold=Prism.util.clone(Prism.languages.markdown.bold); // prettier-ignore
 
-function MarkdownEditor({ file, write }) {
-  console.log(file, write);
+
+function MarkdownEditor({ file, index, files, setFiles }) {
+  const [value, setValue] = useState(initialValue);
+  const fs = files;
+  useEffect(() => {
+    (async () => {
+      const v = await file.text()
+      setValue([
+        {
+          children: [
+            { text: v },
+          ],
+        },
+      ]);
+    })();
+  }, [file]);
+
+  const renderLeaf = useCallback(props => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const decorate = useCallback(([node, path]) => {
+    const ranges = []
+
+    if (!Text.isText(node)) {
+      return ranges;
+    }
+
+    const getLength = token => {
+      if (typeof token === 'string') {
+        return token.length
+      } else if (typeof token.content === 'string') {
+        return token.content.length
+      } else {
+        return token.content.reduce((l, t) => l + getLength(t), 0)
+      }
+    }
+
+    const tokens = Prism.tokenize(node.text, Prism.languages.markdown)
+    let start = 0
+
+    for (const token of tokens) {
+      const length = getLength(token)
+      const end = start + length
+
+      if (typeof token !== 'string') {
+        ranges.push({
+          [token.type]: true,
+          anchor: { path, offset: start },
+          focus: { path, offset: end },
+        })
+      }
+
+      start = end
+    }
+
+    return ranges
+  }, []);
+
   return (
-    <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/markdown</i>
+    <div className={cssEditor.editor}>
+      <Slate editor={editor} value={value} onChange={ value => {
+          const v = value[0].children[0].text;
+          fs[index] = new File([v], fs[index].name, {
+            type: fs[index].type,
+            lastModified: new Date()
+          });
+          setFiles(fs);
+          setValue(value);
+        }}>
+        <Editable
+          decorate={decorate}
+          renderLeaf={renderLeaf}
+          placeholder="Write some markdown..."
+        />
+      </Slate>
     </div>
   );
 }
+
+const Leaf = ({ attributes, children, leaf }) => {
+  if(leaf.url) {
+    console.log(attributes, children, leaf)
+    const text = leaf.text;
+    const regexUrl = /\(.*\)/g;
+    const urls = text.match(regexUrl);
+    if(!urls.length) {
+      return (
+        <span {...attributes}>
+          {children}
+        </span>
+      );
+    }
+    const url = urls[0].substring(1, urls[0].length - 1);
+    const handleClick = e => {
+      e.preventDefault();
+      window.open(url);
+    }
+    return (
+      <a href={url} onClick={handleClick}>
+        <span {...attributes}>
+          {children}
+        </span>
+      </a>
+    );
+  }
+  return (
+    <span
+      {...attributes}
+      className={css`
+        font-weight: ${leaf.bold && 'bold'};
+        font-style: ${leaf.italic && 'italic'};
+        text-decoration: ${leaf.underlined && 'underline'};
+        ${leaf.title &&
+          css`
+            display: inline-block;
+            font-weight: bold;
+            font-size: 20px;
+            margin: 20px 0 10px 0;
+          `}
+        ${leaf.list &&
+          css`
+            padding-left: 10px;
+            font-size: 20px;
+            line-height: 10px;
+          `}
+        ${leaf.hr &&
+          css`
+            display: block;
+            text-align: center;
+            border-bottom: 2px solid #ddd;
+          `}
+        ${leaf.blockquote &&
+          css`
+            display: inline-block;
+            border-left: 2px solid #ddd;
+            padding-left: 10px;
+            color: #aaa;
+            font-style: italic;
+          `}
+        ${leaf.code &&
+          css`
+            font-family: monospace;
+            background-color: #eee;
+            padding: 3px;
+          `}
+      `}
+    >
+      {children}
+    </span>
+  )
+};
+
+const initialValue = [
+  {
+    children: [
+      { text: '' },
+    ],
+  },
+];
 
 MarkdownEditor.propTypes = {
   file: PropTypes.object,
